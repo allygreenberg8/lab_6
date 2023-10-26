ef1a8506742e6fdb47e42f04398330bb23c1ae9f
annawhee407 <ak.wheeler@ufl.edu>
2023-10-26 14:27:26 -0400

    Anna Wheeler decode function for Ally's Repository

--- lab_6_Ally_Greenberg.py
+++ lab_6_Ally_Greenberg.py
@@ -1,12 +1,30 @@
 '''Ally Greenberg: encode function/menu function/main function'''
 def encoder(password):
+    global encoded_string
     encoded_string = ""
     for each in password:
         x = int(each)
         x += 3
         a = str(x)
+        encoded_string
         encoded_string += a
     return encoded_string
+
+
+# decode function by Anna Wheeler
+def decode(encoded_string):
+    decoded_string = ''
+
+    # iterates through string subtracting 3 from each digit
+    for digit in encoded_string:
+        num = int(digit)
+        num -= 3
+        str_num = str(num)
+        decoded_string += str_num
+
+    # returns decoded string
+    return decoded_string
+
 
 def menu():
     print("Menu")
@@ -23,15 +41,14 @@ def main():
 
         if option == "1":
             user_pass = input("Please enter your password to encode: ")
-            encoder(user_pass)
+            encoded_sting = encoder(user_pass)
             print("Your password has been encoded and stored!\n")
+
+        elif option == "2":
+            print(f"The encoded password is {encoder(user_pass)}, and the original password is {decode(encoded_string)}.\n")
 
         else:
             running = False
-
-        '''elif option == "2":
-            print(f"The encoded password is {encoder(user_pass)}, and the original password is {user_pass}.\n")
-        '''
 
 if __name__ == '__main__':
     main()
